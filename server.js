378fa317300fd1b31eeea7bbdec6e24fcfb4c6f3
Akshat Tiwari <tiwariakshat2023it@gmail.com>
2024-08-29 11:39:59 +0530

    node app setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/get', (req, res) => {
+    res.send({ message: 'Nodejs AWS Deployment' });
+});
+
+app.listen(9000, () => {
+    console.log('Server running on port 9000');
+});
